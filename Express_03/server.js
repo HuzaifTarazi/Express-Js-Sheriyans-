c06feb8150991c9124bd362dce9d4ce3c3e86321
Huzaif Tarazi <huzaiftarazi14@gmail.com>
2026-06-26 16:53:30 +0500

    Express_03

--- Express_03/server.js
+++ Express_03/server.js
@@ -1,8 +1,9 @@
 const app = require('./src/app')
 const connectDb = require('./src/database/db')
 
-connectDb()
 
 app.listen(3000, () => {
     console.log("server is running on port 3000")
 })
+
+connectDb()
